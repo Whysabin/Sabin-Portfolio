55d1bda03fbd860e1085fa4ed830c43c71d3d9b6
Whysabin <161411545+Whysabin@users.noreply.github.com>
2024-12-10 19:53:22 +0545

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Sabin-Portfolio
+<h1 align="center" id="title">Portfolio Website V1</h1>
+
+<p id="description">This portfolio website acted as a platform for showcasing my projects providing information about myself and displaying my social media profiles. It featured a terminal-like interface for user interaction.</p>
+
+<h2>🚀 Demo</h2>
+
+[https://lohitkolluri.github.io/Old-Portfolio-Website/](https://lohitkolluri.github.io/Old-Portfolio-Website/)
+
+  
+  
+<h2>🧐 Features</h2>
+
+Here're some of the project's best features:
+
+*   Command Execution and Routing
+*   Command History Navigation
+*   Dynamic Content Printing
+*   Command Line Interface (CLI) Simplicity
+*   External Link Opening
+
+<h2>🛠️ Installation Steps:</h2>
+
+<p>1. Clone the repository:</p>
+
+```
+git clone https://github.com/lohitkolluri/Old-Portfolio-Website
+```
+
+<p>2. Go to the directory:</p>
+
+```
+cd Old-Portfolio-Website
+```
+
+<p>3. Open the index.html file in a web browser</p>
+
+  
+  
+<h2>💻 Built with</h2>
+
+Technologies used in the project:
+
+*   JavaScript
+*   HTML
+*   CSS
+*   Browser DOM
+
+<h2>🛡️ License:</h2>
+
+This project is licensed under the [MIT License](LICENSE)
---
+++ scripts/data.js
@@ -0,0 +1,92 @@
+const linkedin = "https://www.linkedin.com/in/sabin-magar-5966212a5/";
+const github = "https://github.com/Whysabin/";
+const email = "mailto:msabin684@gmail.com";
+const ticTacToeLink = "https://tic-tac-toe-game-bysabin-azure-pi.vercel.app/"; // Replace with actual link
+const sableShareLink = "https://sable-share-p2-p-file-sharing-webpage-using-chunking-algotihm.vercel.app/"; // Replace with actual link
+const virtualWindowLink = "https://sabinwin-portfolio.vercel.app/"; // Replace with actual link
+const ochiCloneLink = "https://ochi-clone-sabin.vercel.app/"; // Replace with actual link
+var today = new Date();
+
+about = [
+  "<br>",
+  "Hello, I'm Sabin! 👋",
+  "<br>",
+  "I'm a Frontend Developer skilled in HTML, CSS, JavaScript, and currently learning React.js.",
+  "<br>",
+  "I focus on creating dynamic, user-friendly web applications for great experiences.",
+  "<br>",
+  "I also enjoy video editing with After Effects and photo editing with Lightroom.",
+  "<br>",
+  "I hold a Bachelor's degree from Ratnarajya Laxmi Campus, blending technical and creative skills.",
+  "<br>",
+  "I'm eager to learn, explore new tech, and collaborate with others.",
+  "<br>",
+  "Feel free to reach out if you'd like to work together or connect!",
+  "<br>",
+];
+
+
+
+banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">(c) Microsoft Corporation. All rights reserved. </span>',
+  "<br>",
+  "███████╗ █████╗ ██████╗ ██╗███╗   ██╗",
+  "██╔════╝██╔══██╗██╔══██╗██║████╗  ██║",
+  "███████╗███████║██████╔╝██║██╔██╗ ██║",
+  "╚════██║██╔══██║██╔═══╝ ██║██║╚██╗██║",
+  "███████║██║  ██║███████╗██║██║ ╚████║",
+  "╚══════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝",
+  "<br>",
+  '<span class="color2">Welcome to my Portfolio Website..</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  "<br>",
+];
+
+
+
+help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>       View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>        View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/SabinMagar' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/Whysabin' +
+    "</a>",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  '<span class="command"><a href="' + ticTacToeLink + '" target="_blank">1. Tic-Tac-Toe</a></span>',
+  "<br>",
+  "<br>",
+  '<span class="command"><a href="' + sableShareLink + '" target="_blank">2. SableShare (P2P-File-Sharing-Webapp)</a></span>',
+  "<br>",
+  "<br>",
+  '<span class="command"><a href="' + virtualWindowLink + '" target="_blank">3. Virtual Window Portfolio</a></span>',
+  "<br>",
+  "<br>",
+  '<span class="command"><a href="' + ochiCloneLink + '" target="_blank">4. Ochi-Clone</a></span>',
+  "<br>",
+  'Click the following links to access all my projects: <a href = "https://github.com/Whysabin" target="_blank">(Github)</a>',
+  "<br>",
+];
